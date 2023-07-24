b06c5d4ffa67bf52f82da6ff9499b0a2d117978d
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2023-07-24 17:43:43 +0300

    back to normal path

--- METEO/read-build-MET-Norway.py
+++ METEO/read-build-MET-Norway.py
@@ -15,8 +15,7 @@ filesNo = 1 + (timeHelper.days*24 + timeHelper.seconds//3600) // 6
 print(f"You have {filesNo} MET-Norway files")
 
 ############################################################################ read first file to build df structure
-#f = open(f'/home/lali/TITAN-ROG-sync/python/METEO/MET-Norway-{firstDate}.json',)
-f = open(f'/home/lali/python/METEO/MET-Norway-{firstDate}.json',)
+f = open(f'/home/lali/TITAN-ROG-sync/python/METEO/MET-Norway-{firstDate}.json',)
 data = json.load(f)
 f.close()
 
@@ -49,8 +48,7 @@ allData = allData.set_index('Date')
 
 for i in range(filesNo):
     fileTime = (datetime.datetime.strptime(firstDate, '%Y-%m-%dT%H:%M:%S') + datetime.timedelta(hours=6*i)).isoformat()
-    #f = open(f'/home/lali/TITAN-ROG-sync/python/METEO/MET-Norway-{fileTime}.json',)
-    f = open(f'/home/lali/python/METEO/MET-Norway-{fileTime}.json',)
+    f = open(f'/home/lali/TITAN-ROG-sync/python/METEO/MET-Norway-{fileTime}.json',)
     data = json.load(f)
     f.close()
     allData.at[fileTime, 'Real'] = data['RO']['temp']
@@ -64,8 +62,7 @@ for i in range(filesNo):
 
 print(allData)
 
-#allData.to_csv('/home/lali/TITAN-ROG-sync/python/METEO/MET-Norway.csv')
-allData.to_csv('/home/lali/python/METEO/MET-Norway.csv')
+allData.to_csv('/home/lali/TITAN-ROG-sync/python/METEO/MET-Norway.csv')
 
 if (True):
     allDataPlot = allData.replace(np.nan,0)
